ef5c58d884b6c07e3f925ca6a6fc49f5affb7695
suneel0101 <suneel0101@gmail.com>
2012-11-21 23:29:19 -0500

    fixing syntaxerror in production settings file

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -1,2 +1,2 @@
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
