643a7afd12471fc5d83a7bcc917e7302b2b7dd09
suneel0101 <suneel0101@gmail.com>
2012-11-22 16:47:16 -0500

    forgot curly brackets for context variable

--- settings/__init__.py
+++ settings/__init__.py
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
-    'project_name',
+    '{{ project_name }}',
 )
 
 # A sample logging configuration. The only tangible logging
