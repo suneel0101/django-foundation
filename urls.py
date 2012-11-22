16e9660c0903d8725f8cdf515e341cf8716f67d4
suneel0101 <suneel0101@gmail.com>
2012-11-22 16:38:37 -0500

    removing underscore from app_name for commandline argument to work, possibly

--- urls.py
+++ urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', '{{ app_name }}.views.home', name='home'),
-    # url(r'^{{ app_name }}/', include('{{ app_name }}.foo.urls')),
+    url(r'^$', '{{ appname }}.views.home', name='home'),
+    # url(r'^{{ appname }}/', include('{{ appname }}.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
