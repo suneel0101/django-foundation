54a70649762d3461070fa75f2be92dc70a229ced
suneel0101 <suneel0101@gmail.com>
2012-11-21 22:55:48 -0500

    adding cwd to sys.path

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 import os
 import sys
 
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+LOCAL_FILE = lambda *path: os.path.join(PROJECT_ROOT, *path)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
     from django.core.management import execute_from_command_line
+    sys.path.insert(0, LOCAL_FILE('..'))
 
     execute_from_command_line(sys.argv)
