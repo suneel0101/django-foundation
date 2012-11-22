a8626f8fe669e78bb47560f51420ed661c50ed04
suneel0101 <suneel0101@gmail.com>
2012-11-22 16:30:23 -0500

    major restructuring of foundation, part 1

--- manage.py
+++ manage.py
@@ -6,9 +6,10 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 LOCAL_FILE = lambda *path: os.path.join(PROJECT_ROOT, *path)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
-    sys.path.insert(0, LOCAL_FILE('{{ project_name }}'))
+    sys.path.insert(0, LOCAL_FILE('apps'))
+    sys.path.insert(0, LOCAL_FILE('..'))
 
     execute_from_command_line(sys.argv)
