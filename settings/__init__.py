9e86b7fdcf5317bf2c5277144cb8525cd6af058d
suneel0101 <suneel0101@gmail.com>
2012-11-22 16:42:32 -0500

    renaming, updating settings, seems as though startproject doesnt take arbitrary command line arguments as promised

--- settings/__init__.py
+++ settings/__init__.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = '{{ project_name }}.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -121,7 +121,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
-    'appname',
+    'project_name',
 )
 
 # A sample logging configuration. The only tangible logging
