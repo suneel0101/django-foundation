6628e707901be6aa3ff468566d2b5e4b26f79918
suneel0101 <suneel0101@gmail.com>
2012-11-21 22:59:56 -0500

    adding project to syspath

--- manage.py
+++ manage.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
     from django.core.management import execute_from_command_line
-    sys.path.insert(0, LOCAL_FILE('..'))
+    sys.path.insert(0, LOCAL_FILE('{{ project_name }}'))
 
     execute_from_command_line(sys.argv)
