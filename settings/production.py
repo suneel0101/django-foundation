da89fc1b35800206302271ec9e2300794c7d4fb8
suneel0101 <suneel0101@gmail.com>
2013-01-02 15:20:27 -0500

    adding AWS vars and setting DEBUG to False in production settings

--- settings/production.py
+++ settings/production.py
@@ -1,2 +1,9 @@
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DEBUG = False
+# Set the following variables if you want to serve static assets from S3.
+# This is recommended.
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_BUCKET_NAME = ''
+MEDIA_URL = 'http://s3.amazonaws.com/{}/'.format(AWS_BUCKET_NAME)
